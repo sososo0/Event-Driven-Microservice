150b14566c7775555f0907bef67dd374ea56e7a9
sohyun <sh0000@pusan.ac.kr>
2024-11-29 23:27:04 +0900

    feat: domain 로직 구현

--- MemberMS/src/main/java/com/msa/member/MemberApplication.java
+++ MemberMS/src/main/java/com/msa/member/MemberApplication.java
@@ -1,5 +1,9 @@
 package com.msa.member;
 
+import com.msa.member.domain.model.Member;
+import com.msa.member.domain.model.vo.Email;
+import com.msa.member.domain.model.vo.PassWord;
+import com.msa.member.domain.model.vo.IDName;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MemberApplication {
 
     public static void main(String[] args) throws Exception {
+        domainTest();
         SpringApplication.run(MemberApplication.class, args);
+    }
+
+    private static void domainTest() throws Exception {
+        System.out.println("------------도메인 모델 테스트 진행------------");
+        System.out.println("회원 생성");
+
+        Member member = Member.registerMember(IDName.sample(), PassWord.sample(), Email.sample());
+        System.out.println(member.toString());
+
+        System.out.println("포인트 10 적립");
+        member.savePoint(10L);
+        System.out.println(member.getPoint());
+
+        System.out.println("포인트 20 적립");
+        member.savePoint(20L);
+        System.out.println(member.getPoint());
+
+        System.out.println("포인트 10 사용");
+        member.usePoint(10L);
+        System.out.println(member.getPoint());
+
+        System.out.println("-----------------------------------------");
     }
 }
--- MemberMS/src/main/java/com/msa/member/domain/model/Member.java
+++ MemberMS/src/main/java/com/msa/member/domain/model/Member.java
@@ -5,6 +5,7 @@ import com.msa.member.domain.model.vo.Email;
 import com.msa.member.domain.model.vo.IDName;
 import com.msa.member.domain.model.vo.MemberPoint;
 import com.msa.member.domain.model.vo.PassWord;
+import com.msa.member.domain.model.vo.UserRole;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -22,4 +23,33 @@ public class Member {
     private Email email;
     private List<Authority> authorities = new ArrayList<Authority>();
     private MemberPoint point;
+
+    public static Member registerMember(IDName idName, PassWord pwd, Email email) {
+        Member member = new Member();
+        member.setIdName(idName);
+        member.setPassword(pwd);
+        member.setEmail(email);
+        member.setPoint(MemberPoint.createPoint());
+        member.addAuthority(new Authority(UserRole.USER));
+        return member;
+    }
+
+    private void addAuthority(Authority authority) {
+        this.authorities.add(authority);
+    }
+
+    public long savePoint(long point) {
+        return this.point.addPoint(point);
+    }
+
+    public long usePoint(long point) throws Exception {
+        return this.point.removePoint(point);
+    }
+
+    public Member login(IDName idName, PassWord pwd) {
+        return this;
+    }
+
+    public void logout(IDName idName) {
+    }
 }
